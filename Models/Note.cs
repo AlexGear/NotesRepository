194007362b974427b3c04c47ae53cc37bd17ed7b
Alex <alexey-bycov@yandex.ru>
2020-03-29 19:18:27 +0700

    Add Note Model

---
+++ Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesRepository.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        
+        [Required]
+        public string Content { get; set; }
+    }
+}
