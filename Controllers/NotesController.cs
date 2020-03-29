84263e4cb7c8b7510a93d0ca0a3dff1113277f4d
Alex <alexey-bycov@yandex.ru>
2020-03-29 23:56:38 +0700

    Add NotesController class skeleton

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotesRepository.Models;
+
+namespace NotesRepository.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly INoteRepository noteRepository;
+
+        public NotesController(INoteRepository noteRepository)
+        {
+            this.noteRepository = noteRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<NoteWithoutContent>> GetAllNotesWithoutContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetNoteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AddNote([FromBody] NoteWithoutId note)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateNote(int id, [FromBody] NoteWithoutId note)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveNote(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
