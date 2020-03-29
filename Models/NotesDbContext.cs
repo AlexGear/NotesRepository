ff893fb5b922277fd2ece60332236619a4751dbf
Alex <alexey-bycov@yandex.ru>
2020-03-29 19:23:01 +0700

    Add NotesDbContext class

---
+++ Models/NotesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotesRepository.Models
+{
+    public class NotesDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
