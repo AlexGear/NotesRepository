ad74bd4bd0f757ecb5fe268b9600736ff686630f
Alex <alexey-bycov@yandex.ru>
2020-03-29 21:25:06 +0700

    Create INoteRepository interface

---
+++ Models/INoteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesRepository.Models
+{
+    interface INoteRepository
+    {
+        Task<Note> GetByIdAsync(int id);
+        Task<List<Note>> GetAllAsync();
+        Task AddAsync(Note note);
+        Task UpdateAsync(Note note);
+        Task RemoveByIdAsync(int id);
+    }
+}
